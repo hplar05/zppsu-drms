aa4f41be9fe1d45d4844dee4413de52ef6084dfa
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2024-08-21 00:27:09 +0800

    fix: vercel build error

--- app/user/dashboard/page.tsx
+++ app/user/dashboard/page.tsx
@@ -10,7 +10,7 @@ const page = async () => {
   if (session?.user) {
     return (
       <div>
-        <h2>welcome to userpage {session?.user?.firstname}</h2>
+        <h2>welcome to userpage {session?.user?.name}</h2>
         <h2>
           <User />
         </h2>

b7b30b4ef240bfb8286a9feb188548e576e20f5e
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2024-08-26 04:33:08 +0800

    update: adjust

--- src/app/admin/(requests)/request-table/page.tsx
+++ src/app/admin/(requests)/request-table/page.tsx
@@ -2,25 +2,30 @@ import { db } from "@/src/lib/db";
 import React from "react";
 import { columns, Request } from "./columns";
 import { DataTable } from "./data-table";
+import NoRequestAvailable from "@/src/components/noRequestAvailable";
 
 async function getRequests(): Promise<Request[]> {
   const res = await db.requestForm.findMany();
-
   return res;
 }
 
 // Page component
 export default async function Page() {
+  const requestCount = await db.requestForm.count();
   const requestData = await getRequests();
 
   return (
     <main className="pt-5 px-5">
-      <div>
-        <h1 className="text-xl md:text-2xl font-bold text-left">
-          All Requests
-        </h1>
-        <DataTable columns={columns} data={requestData} />
-      </div>
+      {requestCount === 0 ? (
+        <NoRequestAvailable />
+      ) : (
+        <div>
+          <h1 className="text-xl md:text-2xl font-bold text-left">
+            All Requests
+          </h1>
+          <DataTable columns={columns} data={requestData} />
+        </div>
+      )}
     </main>
   );
 }

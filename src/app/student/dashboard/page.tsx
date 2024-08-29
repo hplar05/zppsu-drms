d1839ba61c39e338aa6afc8d1ba421133763c73f
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2024-08-29 13:25:10 +0800

    update: disabled button

--- src/app/student/dashboard/page.tsx
+++ src/app/student/dashboard/page.tsx
@@ -7,33 +7,36 @@ import { Tracking } from "./tracking";
 import TrackingTable from "./tracking-table";
 import { db } from "@/src/lib/db";
 
-const page = async () => {
+const Page = async () => {
   const session = await getServerSession(authOptions);
   const userId = session?.user.id;
   const res = await db.requestForm.findMany({
     where: {
       userId: userId,
     },
+    select: {
+      action: true,
+    },
   });
+  const isActionCompletedOrDeclined = res.some(
+    (request) => request.action === "COMPLETED" || request.action === "DECLINE"
+  );
+
   return (
     <div className="h-screen">
       <UserNavbar />
-      <div className=" p-5 m-6 h-[50vh] rounded-md">
+      <div className="p-5 m-6 h-[50vh] rounded-md">
         <div className="flex flex-col justify-center gap-2 w-[40%] pl-10 mb-10">
           <h2 className="text-xl font-bold">Welcome to Dashboard</h2>
         </div>
         <Tracking />
-        <div className="flex flex-col gap-20 justify-center items-center my-20 ">
-          {res.length === 1 ? (
-            <div>
-              <Button disabled={true}>Create Request</Button>
-            </div>
+        <div className="flex flex-col gap-20 justify-center items-center my-20">
+          {!isActionCompletedOrDeclined ? (
+            <Button disabled>Create Request</Button>
           ) : (
-            <div>
-              <Button>
-                <Link href="/student/add-your-request">Create Request</Link>
-              </Button>
-            </div>
+            <Button>
+              <Link href="/student/add-your-request">Create Request</Link>
+            </Button>
           )}
         </div>
         <TrackingTable userId={userId!} />
@@ -42,4 +45,4 @@ const page = async () => {
   );
 };
 
-export default page;
+export default Page;

ac45cd8e36b0cfa710a58e518cd14c70b6abe1e7
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2024-08-31 20:55:38 +0800

    update: reusable validation schema

--- actions/adminRequest.ts
+++ actions/adminRequest.ts
@@ -2,41 +2,32 @@
 
 import { authOptions } from "@/src/lib/auth";
 import { db } from "@/src/lib/db";
+import { AdminCreateRequestSchema } from "@/src/lib/validation/adminCreateRequestSchema";
 import { getServerSession } from "next-auth";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
 
-const RequestSchema = z.object({
-    nameOfStudent: z.string().min(5, "name is required").max(40),
-    studentId: z.string().min(4, "student ID is required").max(40),
-    email: z.string().min(11, "mobile number is required").max(100),
-    mobileNumber: z.string().min(11, "mobile number is required").max(11),
-    course: z.string().min(4, "course is required").max(11),
-    yearAndsection: z.string().min(4, "year and section is required").max(20),
-    purposeOfrequest: z.string().min(6, "purpose of request is required").max(200),
-})
-
 
 //create admin request
 export async function createRequest(formData: FormData) {
+
+    const nameOfStudent = formData.get('nameOfStudent') as string;
+    const studentId = formData.get('studentId') as string;
+    const email = formData.get('email') as string;
+    const mobileNumber = formData.get('mobileNumber') as string;
+    const course = formData.get('course') as string;
+    const yearAndsection = formData.get('yearAndsection') as string;
+    const attachment = formData.get('attachment') as string;
+    const purposeOfrequest = formData.get('purposeOfrequest') as string;
+
+    const session = await getServerSession(authOptions);
+
+    if (!session) return { error: "Unauthorized" }
+    
+    const adminId = session.user.id
  
-    try {
-        const nameOfStudent = formData.get('nameOfStudent') as string;
-        const studentId = formData.get('studentId') as string;
-        const email = formData.get('email') as string;
-        const mobileNumber = formData.get('mobileNumber') as string;
-        const course = formData.get('course') as string;
-        const yearAndsection = formData.get('yearAndsection') as string;
-        const attachment = formData.get('attachment') as string;
-        const purposeOfrequest = formData.get('purposeOfrequest') as string;
-    
-        const session = await getServerSession(authOptions);
-    
-        if (!session) return { error: "Unauthorized" }
-        
-        const adminId = session.user.id
-    
+    try {  
         await db.requestForm.create({
            data: {
             nameOfStudent,
@@ -62,13 +53,12 @@ export async function createRequest(formData: FormData) {
 
 
 //update admin request
-
 export const updateRequest = async (
     id: number,
     prevSate: any,
     formData: FormData
 ) => {
-    const validatedFields = RequestSchema.safeParse(
+    const validatedFields = AdminCreateRequestSchema.safeParse(
         Object.fromEntries(formData.entries())
     );
     const session = await getServerSession(authOptions);
@@ -103,8 +93,6 @@ export const updateRequest = async (
 
 
 //delete admin request
-
-
 export const deleteRequest = async (id: number) => {
     const session = await getServerSession(authOptions);
 
---
+++ src/lib/validation/adminUpdateRequestSchema.ts
@@ -0,0 +1,13 @@
+import { z } from "zod"
+
+export type AdminUpdateRequestType = z.infer<typeof AdminUpdateRequestSchema>
+
+export const AdminUpdateRequestSchema = z.object({
+    nameOfStudent: z.string().min(5, "name is required").max(40),
+    studentId: z.string().min(4, "student ID is required").max(40),
+    email: z.string().min(11, "mobile number is required").max(100),
+    mobileNumber: z.string().min(11, "mobile number is required").max(11),
+    course: z.string().min(4, "course is required").max(11),
+    yearAndsection: z.string().min(4, "year and section is required").max(20),
+    purposeOfrequest: z.string().min(6, "purpose of request is required").max(200),
+})

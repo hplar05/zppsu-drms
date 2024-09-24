d0c6de3d8d831f9cb921510b735f2124483585f3
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2024-09-24 22:09:56 +0800

    fix: in admin validation schema

--- src/lib/validation/adminCreateRequestSchema.ts
+++ src/lib/validation/adminCreateRequestSchema.ts
@@ -11,7 +11,7 @@ export const AdminCreateRequestSchema = z.object({
       .refine((email) => email.length <= 255, { message: "Email is too long" }),
     mobileNumber: z.string().min(11, "Mobile Number is required"),
     course: z.string().min(4, "Course is required").max(40),
-    yearAndsection: z.string().min(4, "Year & Section is required").max(40),
+    yearAndsection: z.string().min(2, "Year & Section is required").max(40),
     // subjectname: z.string().min(4, "Subject name is required").max(100),
     purposeOfrequest: z.string().min(5, "Purpose is required").max(100),
     requestChoices: z.string().min(2).max(50),
--- src/lib/validation/adminUpdateRequestSchema.ts
+++ src/lib/validation/adminUpdateRequestSchema.ts
@@ -8,6 +8,6 @@ export const AdminUpdateRequestSchema = z.object({
     email: z.string().min(11, "mobile number is required").max(100),
     mobileNumber: z.string().min(11, "mobile number is required").max(11),
     course: z.string().min(4, "course is required").max(11),
-    yearAndsection: z.string().min(4, "year and section is required").max(20),
+    yearAndsection: z.string().min(2, "year and section is required").max(40),
     purposeOfrequest: z.string().min(6, "purpose of request is required").max(200),
 })

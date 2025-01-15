4c9b921e174f0a96db8436f21abdccc843487c3d
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2025-01-15 16:07:18 +0800

    update: added navbar in admin chat page

--- src/app/admin/chats/page.tsx
+++ src/app/admin/chats/page.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import AdminNavbar from "../_components/adminNavbar";
 
 const page = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen">Hi</div>
+    <div className="min-h-screen bg-gray-50 dark:bg-[#121212] mt-2">
+      <div className="max-md:hidden block">
+        <AdminNavbar />
+        <div className="flex items-center justify-center min-h-screen">
+          <h1>Under Development!</h1>
+        </div>
+      </div>
+    </div>
   );
 };
 
